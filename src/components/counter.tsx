52e934f2ee2ff9278fe83786b4159611ca4d57f8
lgorniak <lukasgorniak@gmail.com>
2022-04-05 16:55:19 +0200

    adding embedding-expressions examle

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+	state = {
+		count: 0,
+	};
+
 	render() {
 		return (
 			<React.Fragment>
-				<h1>Hello World</h1>
+				<h1>{this.formatCount()}</h1>
 				<button>Increment</button>
 			</React.Fragment>
 		);
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
 	}
 }
 
