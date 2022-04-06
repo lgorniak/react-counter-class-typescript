caa6023d169e4c8c55838aac1d15dca42ff1d896
lgorniak <lukasgorniak@gmail.com>
2022-04-06 09:35:50 +0200

    initalize state inside a constructor

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import { Count } from "../types/count";
 
-class Counter extends Component {
-	state: Count = {
-		number: 0,
-	};
+interface IProps {}
 
-	render() {
-		return (
-			<React.Fragment>
-				<h1>{this.format()}</h1>
-				<button>Increment</button>
-			</React.Fragment>
-		);
+class Counter extends Component<IProps, Count> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			number: 0,
+		};
 	}
 
 	format = (): number | string => {
 		const { number } = this.state;
 		return number === 0 ? "Zero" : number;
 	};
+
+	iterateCounter = (): void => {
+		if (this.state) this.setState({ number: this.state.number + 1 });
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1>{this.format()}</h1>
+				<button onClick={this.iterateCounter}>Increment</button>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Counter;
